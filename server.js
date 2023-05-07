17636293592f992ca3009dcf6310814d6adb2fd2
Gabriel Couto de Freitas <gabriel.freitas.pb@compasso.com.br>
2023-05-07 13:50:37 -0300

    styles: titulo e autores dos gifs aparecendo na tela

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path    = require('path');
 const express = require('express');
 const hbs1     = require('express-handlebars');
 const app     = express();
-const port    = 3001;
+const port    = 5000;
 const api_key = '3rE2j7mlap2LyDvbcqXY3yywtdNwhPBk';
 
 app.engine('hbs', hbs1.engine({
@@ -35,6 +35,27 @@ app.get('/', (req,res)=>{
         });
     });
 });
+
+// app.get('/', (req,res)=>{
+//     // let queryString = req.query.term;
+//     // let limit       = req.query.limit || 10;
+//     // let term        = encodeURIComponent(queryString);
+//    //  let url         = http://api.giphy.com/v1/gifs/search?q=${term}&api_key=${api_key}&limit=${limit}&rating=pg-13
+//     let url = `http://api.giphy.com/v1/gifs/random?api_key=${api_key}`
+
+//     http.get(url,(response)=>{
+//         response.setEncoding('utf-8');
+//         let body='';
+
+//         response.on('data', (d)=>{
+//             body+=d;
+//         });
+//         response.on('end',()=>{
+//             let parsed = JSON.parse(body);
+//             res.render('search-giphy', {gifs: parsed.data})
+//         });
+//     });
+// });
 
 app.listen(port, ()=>{
     console.log("Listening na porta "+port);
