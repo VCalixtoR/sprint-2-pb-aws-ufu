b39a94fd142597b9203bb2f6b4e0be2785df3daf
pdodonto <pdodonto@ufu.br>
2023-05-07 16:23:05 -0300

    style: changes in main.handlers and overall css

--- README.md
+++ README.md
@@ -2,10 +2,36 @@
 
 ## Sobre o Repositório
 
+Este repositório possui uma solução para a segunda avaliação do curso machine learning para AWS da Compass UOL. O objetivo é a implementação de um sistema que funciona como intermediário de uma API pública. Foram abordadas os frameworks ... e bibliotecas ... além de boas práticas e padrões de projetos web para a execução da atividade.
+
 ## Início Rápido
+
+Para visualizar a aplicação disponibilizada na web acesse:
+* ...
+
+Para executar o código localmente:
+1. Clone este repositório
+2. Faça um checkout para esta branch
+3. Instale as dependencias do node utilizando: $ npm install
+3. Execute o projeto node pelo script dev: $ npm run dev
+
+Observações:
+1. Verifique se a versão do node instalada é igual ou superior a v18.16.0
 
 ## Funcionamento
 
+Ao iniciar ...
+
 ## Do Desenvolvimento da Atividade
 
+Inicialmente foi definido o template de pastas utilizando html5 boilerplate:
+
+* [html5 boilerplate](https://github.com/h5bp/html5-boilerplate/)
+
+Definido as padronizações, foi desenvolvido ...
+
+Por fim, este README foi criado, modificado e a branch foi enviada ao repositório remoto.
+
 ## Dificuldades
+
+Não foram encontradas dificuldades ao realizar a implementação.
--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,41 @@ const app = express();
 const exphbs = require('express-handlebars');
 const path = require('path');
 const bodyParser = require('body-parser');
+
+const PORT = 8000;
+const PROJECT_PATH = process.cwd();
+const VIEWS_PATH = path.join(PROJECT_PATH, 'src', 'views');
+const STATIC_PATH = path.join(PROJECT_PATH, 'src', 'public');
+const ROUTES_PATH = path.join(PROJECT_PATH, 'src', 'routes');
+
+// body parser and express
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+// index file for default endpoint
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+// handle bars
+app.set('views', VIEWS_PATH);
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//helpers
+const Handlebars = require('handlebars');
+Handlebars.registerHelper('limit', function(arr, limit) {
+  if (!Array.isArray(arr)) { return []; }
+  return arr.slice(0, limit);
+});
+
+// default static folder - used in html rendering
+app.use(express.static(STATIC_PATH));
+
+// listening port
+app.listen(PORT, function(){
+  console.log(`Working in: http://localhost:${PORT}/`);
+});
+
+// routes
+app.use('/card', require(path.join(ROUTES_PATH, 'cards.js')));
