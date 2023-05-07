35a92fbe1080dc0332bb0945b89bdf359eefa0fd
Gabriel Couto de Freitas <gabriel.freitas.pb@compasso.com.br>
2023-05-07 14:02:17 -0300

    style: mudança no nome de arquivos e textos do html

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const http    = require('http');
 const path    = require('path');
 const express = require('express');
-const hbs1     = require('express-handlebars');
+const hbs1    = require('express-handlebars');
 const app     = express();
-const port    = 5000;
+const port    = 3000;
 const api_key = '3rE2j7mlap2LyDvbcqXY3yywtdNwhPBk';
 
 app.engine('hbs', hbs1.engine({
@@ -31,16 +31,12 @@ app.get('/', (req,res)=>{
         });
         response.on('end',()=>{
             let parsed = JSON.parse(body);
-            res.render('search-giphy', {gifs: parsed.data})
+            res.render('giphy-search', {gifs: parsed.data})
         });
     });
 });
 
 // app.get('/', (req,res)=>{
-//     // let queryString = req.query.term;
-//     // let limit       = req.query.limit || 10;
-//     // let term        = encodeURIComponent(queryString);
-//    //  let url         = http://api.giphy.com/v1/gifs/search?q=${term}&api_key=${api_key}&limit=${limit}&rating=pg-13
 //     let url = `http://api.giphy.com/v1/gifs/random?api_key=${api_key}`
 
 //     http.get(url,(response)=>{
