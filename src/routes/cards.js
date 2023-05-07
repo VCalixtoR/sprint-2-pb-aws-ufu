cda021d74e571d815363fbf9e600430da6ddeec5
Vinicius Calixto <viniciuscalixto.prof@gmail.com>
2023-05-07 16:27:18 -0300

    feat: added request handling async function

--- src/routes/cards.js
+++ src/routes/cards.js
@@ -1,4 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
+const BASE_HANDWRYTTEN_URL = 'https://api.handwrytten.com/';
+
+// fetches a request with exception handling
+async function baseRequestHandling(options, base_url, endpoint){
+
+  let rawResponse = {};
+  let response = {};
+
+  try{
+    rawResponse = await fetch(`${base_url}${endpoint}`, options).then((response) => {
+      return response;
+    }).catch( _ => {
+      throw new Error('Não foi possível conectar com o servidor');
+    });
+
+    // if response is ok, awaits for text conversion and after converts to json
+    // needed to avoid json parsing empty object exception
+    if(rawResponse.ok){
+      response = await rawResponse.text();
+      if(response){
+        response = JSON.parse(response);
+      }
+      return response;
+    }
+  }
+  catch(error){
+    console.log(error.message);
+    return error;
+  }
+}
+
 module.exports = router;
